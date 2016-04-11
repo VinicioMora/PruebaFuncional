4ad88940d25427af55605a5545d9adb62ffe9776
juliocalderon <andres.ca1511@gmail.com>
2016-04-11 11:41:29 -0600

    actualizacion

---
+++ www/js/coneccioncouch.js
@@ -0,0 +1,7 @@
+$.couch.urlPrefix = "http://52.26.180.68:5984";
+
+$.couch.info({
+	success: function(data) {
+		console.log(data);
+	}
+});
---
+++ www/js/validaciones.js
@@ -0,0 +1,9 @@
+funtion validarentrada(){
+
+	var imput = document.getElementById('email');
+
+	if(imput.value.lenght == 0){
+		alert("Error espacio vacio");
+	}
+
+}
